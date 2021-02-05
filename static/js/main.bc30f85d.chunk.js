375476138e1937f4ad2248a4373d73576375afcb
Vani-lla <igorkowaczyk24@gmail.com>
2021-02-05 12:32:31 +0100

    Updates

--- static/js/main.bc30f85d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpkoper=this.webpackJsonpkoper||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);t(1);var c=t(3),s=t.n(c),r=(t(8),t(9),t(0));var a=function(){return Object(r.jsx)("div",{className:"App",children:Object(r.jsx)("h1",{className:"jd",children:"Mati pajac"})})};s.a.render(Object(r.jsx)(a,{}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.bc30f85d.chunk.js.map

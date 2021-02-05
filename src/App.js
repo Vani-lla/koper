cc4ea5e041f56359e6fa1a1c9bd5f9612ac64b42
Vani-lla <igorkowaczyk24@gmail.com>
2021-02-05 12:36:39 +0100

    Basic modification

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
    return (
       <div className="App">
-         <h1>Aisaretai!</h1>
+         <h1 className='jd'>Mati to kozak kolega</h1>
       </div>
    );
 }
